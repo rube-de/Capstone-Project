6ee84c43e1fd5af43c8c0afbc67d21241786cda7
rube-de <rube-de@users.noreply.github.com>
2016-12-20 15:03:33 +0100

    add prod after scan

--- app/src/main/java/de/ruf2/rube/fridgeorganizer/ScanProductFragment.java
+++ app/src/main/java/de/ruf2/rube/fridgeorganizer/ScanProductFragment.java
@@ -1,30 +1,50 @@
 package de.ruf2.rube.fridgeorganizer;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.trello.rxlifecycle.components.support.RxFragment;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.ruf2.rube.fridgeorganizer.data.entities.Fridge;
+import de.ruf2.rube.fridgeorganizer.data.entities.Product;
+import io.realm.Realm;
+import io.realm.RealmResults;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,15 +54,10 @@ import rx.schedulers.Schedulers;
  * Use the {@link ScanProductFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ScanProductFragment extends RxFragment implements Observer<String> {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ScanProductFragment extends RxFragment implements Observer<String>,View.OnClickListener{
+    private static final String ARG_EAN = "ean";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String mEan;
 
     private OnFragmentInteractionListener mListener;
 
@@ -52,10 +67,27 @@ public class ScanProductFragment extends RxFragment implements Observer<String> 
     EditText mEditTextEan;
     @Bind(R.id.button_scan)
     Button mButtonScan;
+    @Bind(R.id.edit_text_buy_date)
+    EditText mEditTextBuyDate;
+    @Bind(R.id.edit_text_expiry_date)
+    EditText mEditTextExpiryDate;
     @Bind(R.id.edit_text_product_name)
     EditText mEditTextProductName;
+    @Bind(R.id.edit_text_product_amount)
+    EditText mEditTextProductAmount;
+    @Bind(R.id.spinner_fridge)
+    Spinner mSpinnerFridge;
+    @Bind(R.id.button_new_fridge_product_fragment)
+    Button mButtonNewFridge;
 
     private final String EAN_CONTENT = "eanContent";
+
+    private Realm mRealm;
+
+    private Activity mContext;
+
+    private DatePickerDialog mBuyDatePickerDialog;
+    private DatePickerDialog mExpiryDatePickerDialog;
 
     public ScanProductFragment() {
         // Required empty public constructor
@@ -65,16 +97,14 @@ public class ScanProductFragment extends RxFragment implements Observer<String> 
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param ean Parameter 1.
      * @return A new instance of fragment ScanProductFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ScanProductFragment newInstance(String param1, String param2) {
+    public static ScanProductFragment newInstance(String ean) {
         ScanProductFragment fragment = new ScanProductFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_EAN, ean);
         fragment.setArguments(args);
         return fragment;
     }
@@ -83,9 +113,10 @@ public class ScanProductFragment extends RxFragment implements Observer<String> 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mEan = getArguments().getString(ARG_EAN);
         }
+        mContext = getActivity();
+        mRealm = Realm.getDefaultInstance();
     }
 
     @Override
@@ -103,6 +134,22 @@ public class ScanProductFragment extends RxFragment implements Observer<String> 
         View fragmentView = inflater.inflate(R.layout.fragment_scan_product, container, false);
         ButterKnife.bind(this, fragmentView);
 
+        mRealm = Realm.getDefaultInstance();
+
+
+        setDateTimeField();
+
+        //init fridge spinner
+        RealmResults<Fridge> fridges = mRealm.where(Fridge.class).findAll();
+        ArrayAdapter<Fridge> fridgeAdapter = new ArrayAdapter<>(getActivity(), R.layout.sipmle_spinner_dropdown_item, fridges);
+        fridgeAdapter.setDropDownViewResource(R.layout.sipmle_spinner_dropdown_item);
+        mSpinnerFridge.setAdapter(fridgeAdapter);
+
+        //init dates
+        mEditTextBuyDate.setText(Utilities.getTodayDateString());
+        mEditTextExpiryDate.setText(Utilities.getTodayDateString());
+
+
         mEditTextEan.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -118,20 +165,19 @@ public class ScanProductFragment extends RxFragment implements Observer<String> 
             public void afterTextChanged(Editable s) {
                 String ean = s.toString();
 
+                //get product name from ean service
                 cache = getStringObservable();
-
                 cache
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(ScanProductFragment.this);
 
-                //Once we have an EAN, start a product intent
-//                Intent productIntent = new Intent(getActivity(), ProductService.class);
-//                productIntent.putExtra(ProductService.EAN, ean);
-//                productIntent.setAction(ProductService.FETCH_PRODUCT);
-//                getActivity().startService(productIntent);
-//                AddBook.this.restartLoader();
             }
         });
+
+        //get ean from init
+        if(!TextUtils.isEmpty(mEan)) {
+            mEditTextEan.setText(mEan);
+        }
 
         mButtonScan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -169,6 +215,76 @@ public class ScanProductFragment extends RxFragment implements Observer<String> 
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+        mRealm.close();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == mEditTextBuyDate) {
+            mBuyDatePickerDialog.show();
+        } else if (view == mEditTextExpiryDate) {
+            mExpiryDatePickerDialog.show();
+        }
+    }
+
+    @OnClick(R.id.button_add_product)
+    public void onClickAddProduct(View view) {
+        Timber.d("adding product");
+        Utilities.hideKeyboard(mContext);
+        try {
+            //get product values
+            String productName = mEditTextProductName.getText().toString();
+            Date buyDate = Utilities.parseDate(mEditTextBuyDate.getText().toString());
+            Date expireDate = Utilities.parseDate(mEditTextExpiryDate.getText().toString());
+            Integer amount = NumberUtils.toInt(mEditTextProductAmount.getText().toString(), 0);
+            Fridge fridge = (Fridge) mSpinnerFridge.getSelectedItem();
+
+            if (productName.isEmpty() ||  amount == 0) {
+                if (productName.isEmpty()) {
+                    mEditTextProductName.setError(getString(R.string.field_required));
+                }
+                if (amount == 0) {
+                    mEditTextProductAmount.setError(getString(R.string.error_product_amount));
+                }
+            } else {
+                //Set fields
+                Product product = new Product();
+                product.setName(productName);
+                product.setAmount(amount);
+                product.setBuyDate(buyDate);
+                product.setExpiryDate(expireDate);
+                product.setFridge(fridge);
+
+                // Get reference to writable database
+                mRealm.beginTransaction();
+                //Create realm object
+                mRealm.copyToRealm(product);
+                //insert fridge into db
+                mRealm.commitTransaction();
+
+                Snackbar.make(view, "new product created: " + productName, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @OnClick(R.id.button_new_fridge_product_fragment)
+    public void onClickNewFridge(View view) {
+        AddFridgeFragment newFragment = new AddFridgeFragment();
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container_product, newFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     @Override
@@ -224,6 +340,32 @@ public class ScanProductFragment extends RxFragment implements Observer<String> 
                 subscriber.onCompleted();
             }
         }).subscribeOn(Schedulers.io());
+    }
+
+    private void setDateTimeField() {
+        mEditTextExpiryDate.setOnClickListener(this);
+        mEditTextBuyDate.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        mBuyDatePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                mEditTextBuyDate.setText(Utilities.getDateString(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        mExpiryDatePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                mEditTextExpiryDate.setText(Utilities.getDateString(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
     }
 
 
