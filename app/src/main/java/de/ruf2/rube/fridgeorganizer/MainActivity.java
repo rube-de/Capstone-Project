6b282b4441656ee1d5577e238f254c306b3ad70b
rube-de <rube-de@users.noreply.github.com>
2016-12-22 17:17:36 +0100

    rmv icons on drawer

--- app/src/main/java/de/ruf2/rube/fridgeorganizer/MainActivity.java
+++ app/src/main/java/de/ruf2/rube/fridgeorganizer/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -224,6 +225,7 @@ public class MainActivity extends AppCompatActivity implements AddProductFragmen
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        View v = item.getActionView();
 
         if (id == R.id.nav_search_product) {
             Timber.d("navi search product");
@@ -270,8 +272,12 @@ public class MainActivity extends AppCompatActivity implements AddProductFragmen
             transaction.commit();
             mTitle = getString(R.string.title_settings);
         } else if (id == R.id.nav_share) {
+            Snackbar.make(v, "navigation share is not yet implemented", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
 
         } else if (id == R.id.nav_send) {
+            Snackbar.make(v, "navigation send is not yet implemented" , Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
 
         }
 //        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
